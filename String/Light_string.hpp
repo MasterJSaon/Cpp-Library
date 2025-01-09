257cd485cf21dca3e165a4b7bdcfc7cdb3e14bc2
FiJay <filipszeliga441@gmail.com>
2025-01-09 16:36:37 +0100

    string now use functions from namesp

--- String/Light_string.hpp
+++ String/Light_string.hpp
@@ -12,20 +12,12 @@ namespace lsstr {
 class Light_string {
      private:
           char *str;
-          bool isSetup;
-          size_t (*len_func)(const char *);
-
-          void setup() {
-               isSetup = true;
-               this -> len_func = lsstr::len;
-          }
      public:
-          Light_string(): len_func(nullptr), isSetup(false) {
+          Light_string() {
                this -> str = new char[1];
                str[0] = '\0';
           }
           explicit Light_string(const char * chain) {
-               setup();
                size_t arg_len = strlen(chain);
                this -> str = new char[arg_len + 1];
                strcpy(this -> str, chain);                  // strcpy cares about null terminator
@@ -40,16 +32,9 @@ class Light_string {
           };
 
           size_t length() const {
-               if (isSetup) {
-                    return this -> len_func(this -> str);
-               } else {
-                    return 0;
-               }
+               return lsstr::len(this->str);
+               // return strlen(this -> str);
           }
-
-          // const size_t length() const {
-          //      return strlen(this -> str);
-          // }
 
           const char * c_str() {
                return this -> str;
